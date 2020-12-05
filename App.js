31764d860ae41baa24e0d5cb33ee78868c05b470
Pawan Kumar <pk.soft29@gmail.com>
2020-12-05 19:36:18 +0530

    Added : projected file

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+   StyleSheet,
+  ScrollView,
+  View,
+  Text
+} from 'react-native';
+
+export default class App extends Component{
+  render(){
+    return (
+      <>
+        
+      </>
+    )
+  }
+  
+};
+
+const styles = StyleSheet.create({
+  container : {
+    height : '10%',
+    backgroundColor : "#52667a",
+    alignContent : 'center',
+  },
+  footer_container : {
+
+    height : '7%',
+    backgroundColor : "#52667a",
+  }
+});
+
